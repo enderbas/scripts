fe2dde45062f54e9b92d9a91714916d1a393fdd4
Engin Deniz Erbas <engin.erbas@sparsetechnology.com>
2022-12-01 16:14:14 +0300

    New options add
    - -a option appends new directories on existings
    - -n preffered path name option add

--- r-file-creation.sh
+++ r-file-creation.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
-while getopts p:i:n:h flag
+function get_last() {
+    max=0
+    for dir in $basepath/*; do
+        num=$(echo $dir | tr -dc '0-9')
+        if (( num > max )); then
+            max=$num
+        fi
+    done
+    echo $max
+}
+
+function format_path() {
+    lastchar=$(echo "${basepath:(-1)}")
+    if [[ $lastchar == "/" ]]; then
+        basepath=${basepath::-1}
+    fi
+}
+
+append=false;
+opterr=0
+while getopts p:i:n:ha flag
 do
     case "$flag" in
         p) basepath=${OPTARG};;
         i) iteration=${OPTARG};;
-        h) 
-            echo "-p : Pathname under which paths will be created"
-            echo "-i : Iteration count for how many paths want to create."
-            exit
+        h) cat << EOF
+Usage: create-template.sh [OPTION]...
+
+-p : Pathname for directory creation
+-i : Iteration count for how many paths want to create.
+-a : Append -i count of new file
+-n : preffered path name. Default value: problem
+EOF
+exit
         ;;
-        n) name=${OPTARG};
+        n) name=${OPTARG};;
+        a) append=true;;
     esac
 done
 
-if [[ $iteration == 0 ]]; then
-    echo "iteration cannot be 0"
+if [[ -z $iteration || -z $basepath ]]; then
+    echo "Iteration and Path must be specified. Use -i and -p options."
     exit
 fi
 
@@ -23,11 +49,25 @@ if [[ -z $name ]]; then
     name="problem"
 fi
 
-for ((i = 0 ; i < $iteration ; i++)); do
-    fname=$name$i
-    path=$basepath/$fname
-    echo "Created path: $path"
-    mkdir $path
-    cp .main $path/main.c
-    cp .rdme $path/README.md
+format_path
+
+begin=1
+if [[ $append == true ]]; then
+    max=$(get_last)
+    iteration=$((iteration+$max+1))
+    begin=$((max+1))
+    echo iteration $iteration
+    echo begin $begin
+else
+    iteration=$((iteration+1))
+    echo iteration $iteration
+fi
+
+for ((i = begin ; i < $iteration ; i++)); do
+        fname=$name$i
+        path=$basepath/$fname
+        echo "Created path: $path"
+        mkdir $path
+        cp .main $path/main.c
+        cp .rdme $path/README.md
 done
